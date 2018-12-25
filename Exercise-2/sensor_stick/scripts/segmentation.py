92d7ef98834c11425ff714df2ae62359737e9a0b
Luis Felipe Wolf Batista <luisfelipewb@gmail.com>
2018-12-25 22:25:42 +0900

    Add Euclidean segmentation
    Add functions for euclidean segmentation
    Necessary to adjust the parameters in oder to be succesfully create the clusters

--- Exercise-2/sensor_stick/scripts/segmentation.py
+++ Exercise-2/sensor_stick/scripts/segmentation.py
@@ -34,17 +34,49 @@ def pcl_callback(pcl_msg):
     cloud_objects = cloud_filtered.extract(inliers, negative=True)
 
     # TODO: Euclidean Clustering
+    white_cloud = XYZRGB_to_XYZ(cloud_objects)
+
+    print(white_cloud)
+    tree = white_cloud.make_kdtree()
+    #print(tree)
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    #print(ec)
+    ec.set_ClusterTolerance(0.05)
+    ec.set_MinClusterSize(10)
+    #ec.set_MaxClusterSize(250)
+    ec.set_SearchMethod(tree)
+    cluster_indices = ec.Extract()
+    #print(cluster_indices)
 
     # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    cluster_color = get_color_list(len(cluster_indices))
+    #print("cluster_color")
+    #print(cluster_color)
+    color_cluster_point_list = []
+
+    for j, indices in enumerate(cluster_indices):
+        for i, indice in enumerate(indices):
+            #print("for ",i,j)
+            color_cluster_point_list.append([white_cloud[indice][0],
+                                             white_cloud[indice][1],
+                                             white_cloud[indice][2],
+                                             rgb_to_float(cluster_color[j])])
+    cluster_cloud = pcl.PointCloud_PointXYZRGB()
+    cluster_cloud.from_list(color_cluster_point_list)
+    print (cluster_cloud)
+    filename = 'cluster_cloud.pcd'
+    pcl.save(cluster_cloud, filename)
 
     # TODO: Convert PCL data to ROS messages
     ros_cloud_object = pcl_to_ros(cloud_objects)
     ros_cloud_table = pcl_to_ros(cloud_table)
+    ros_cluster_cloud = pcl_to_ros(cluster_cloud)
 
     # TODO: Publish ROS messages
 
     pcl_objects_pub.publish(ros_cloud_object)
     pcl_table_pub.publish(ros_cloud_table)
+    pcl_cluster_pub.publish(ros_cluster_cloud)
 
 if __name__ == '__main__':
 
@@ -57,6 +89,7 @@ if __name__ == '__main__':
     # TODO: Create Publishers
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
+    pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
 
     # Initialize color_list
     get_color_list.color_list = []
